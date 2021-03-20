74e4321ecbdbe4f8aad6c2fc78c116fb61d4ed19
Robin Lunde <robin@wub.no>
2021-03-20 19:21:25 +0100

    Working shell of an android app

--- Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
+++ Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
@@ -1,40 +1,95 @@
 package com.robinlunde.mailbox
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        //val toasted = Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT)
+        // Show toast
+        //toasted.show()
+
+        showStatus();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.my_menubar, menu)
+        // return true so that the menu pop up is opened
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logo -> {
+                showStatus()
+            }
+            R.id.logs -> {
+                showLogs()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showStatus(): Boolean {
+        setContentView(R.layout.activity_main)
         // Get time variables for text from view
+        val timestampText = findViewById<TextView>(R.id.timestamp_text)
         val timestampTime = findViewById<TextView>(R.id.timestamp_time)
         val timestampDay = findViewById<TextView>(R.id.timestamp_day)
         // Get button from view
-        val button = findViewById<Button>(R.id.Button)
+        val button = findViewById<Button>(R.id.button)
+        // Get icon from view
+        val icon = findViewById<ImageView>(R.id.post_box)
         // Get values from Bluetooth
         // todo
+        // val timestamp = getValFromBT()
+
         // If value received, show push notification!
         // if pressed, shows main view - updated
         // todo
         // Parse string from BT - Split into time and day
         // todo
-        // Set values in variables for timestamps
-        timestampTime.text = "hh:mm:ss"
-        timestampDay.text = "12.12.2012"
+        //val timestamp = "21:12:00 12.12.2012"
+        val timestamp = ""
+        if (timestamp != ""){
+            var timestampParts = timestamp.split(" ")
+            Log.d("test", timestampParts.toString())
+            timestampDay.text = timestampParts[1]
+            timestampTime.text = timestampParts[0]
+
+        } else {
+            timestampText.text = "No new post received!"
+            timestampDay.visibility = View.INVISIBLE
+            timestampTime.visibility = View.INVISIBLE
+            button.visibility = View.INVISIBLE
+            icon.visibility = View.VISIBLE
+        }
 
         // Register activity for button
         // What happens when button is clicked
         button.setOnClickListener{
             // Send reply by BT
             // TODO
-            val toasted = Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT)
-            // Show toast
-            toasted.show()
+            showLogs()
         }
+        return true;
+    }
+
+    private fun showLogs(): Boolean {
+        // Show a different view!
+        setContentView(R.layout.activity_log)
+        return true;
     }
 }
