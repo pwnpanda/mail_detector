954b419763ea7d0cef4e8477784ab5e5ea0ffff5
Robin Lunde <robin@teamcyber.no>
2021-03-21 23:35:35 +0100

    Working util script! Needs testing

--- WebAPI/mail_api/start_util.sh
+++ WebAPI/mail_api/start_util.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 date=$(date +"%Y-%m-%d")
-PATH = "tmp/pids/server.pid"
-if [[ ! -f $PATH ]]; then
-    rails s -p 12121 -b 127.0.0.1 -e production > /tmp/rails_server.log 2>&1 &
-else if [[ ps -p $(cat $PATH)  ]]; then
+file_path="tmp/pids/server.pid"
+
+function doCheck {
+    if [[ ! -f $file_path ]]; then
+        return 1
+    fi
+    PID=$(cat $file_path)
+    echo $PID
+    RES=$(ps -p $PID)
+    STATUS=$($?)
+    echo $STATUS
+    return $STATUS
+}
+
+if [ doCheck == 0 ]; then
     echo "Already running!"
+elif [[ ! -f $file_path ]]; then
+    rails s -p 12121 -b 127.0.0.1 -e production > /tmp/rails_server.log 2>&1 &
 else
     python3 /var/www/slackbot/alert.py "\n>*Ruby - API Server Down!*\n*Time:* $(date)"
 fi
