c1dac65d18e143ff84addb02a4bac375d40fc840
Robin Lunde <robin@teamcyber.no>
2021-03-21 23:12:23 +0100

    Util startup script

--- WebAPI/mail_api/start_util.sh
+++ WebAPI/mail_api/start_util.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-# Start rails
-# Check that it runs
-# If it stops, restart
-# If it cannot restart, send slack warning
+date=$(date +"%Y-%m-%d")
+PATH = "tmp/pids/server.pid"
+if [[ ! -f $PATH ]]; then
+    rails s -p 12121 -b 127.0.0.1 -e production > /tmp/rails_server.log 2>&1 &
+else if [[ ps -p $(cat $PATH)  ]]; then
+    echo "Already running!"
+else
+    python3 /var/www/slackbot/alert.py "\n>*Ruby - API Server Down!*\n*Time:* $(date)"
+fi
