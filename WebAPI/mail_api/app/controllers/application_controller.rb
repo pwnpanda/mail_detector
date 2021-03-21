9047379f184aa7fe6c0419b8b6738c303004e33d
Robin Lunde <robin@wub.no>
2021-03-21 12:45:34 +0100

    Ruby on Rails Web API fully working

--- WebAPI/mail_api/app/controllers/application_controller.rb
+++ WebAPI/mail_api/app/controllers/application_controller.rb
@@ -1,22 +1,25 @@
 class ApplicationController < ActionController::API
-    def store
+    #def store
+        # reqire 'date' ?
         # get json data from post data
         # field named timestamp
-        if params[:timestamp] then
-            timestamp = params[:timestamp]
-            puts "Params in:"
-            puts timestamp
+        #if params[:timestamp] then
+        #    timestamp = params[:timestamp]
+        #    puts "Params in:"
+        #    puts timestamp
+        #    ts = DateTime.parse(timestamp)
+        #    puts ts
             # store json data in db
             # Reply with 200 OK
-        else
+        #else
             # Give 400 if not correct format
-            head 400
-        end
-    end
-    def get
+        #    head 400
+        #end
+    #end
+    #def get
         # get json data from db
         #something
-        # send reply
-        @data = "jsondata"
-    end
+        # send reply any data as json
+        #@data = "jsondata"
+    #end
 end
