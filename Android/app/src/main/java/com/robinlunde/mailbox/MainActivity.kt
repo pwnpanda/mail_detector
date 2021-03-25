89b8c62b3653c14affb0ef0ed920d7a9f818d7ca
Robin Lunde <robin@teamcyber.no>
2021-03-26 00:12:24 +0100

    Working functioncalls for menubar in main

--- Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
+++ Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.robinlunde.mailbox
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 import com.robinlunde.mailbox.databinding.ActivityMainBinding
 
 // TODO
@@ -17,51 +16,54 @@ import com.robinlunde.mailbox.databinding.ActivityMainBinding
 //     showAlert();
 
 class MainActivity() : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-            super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-            //setContentView(R.layout.activity_main)
-            val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-
-            // TODO init httprequests here
-            //val navController = this.findNavController(R.id.navigation)
-            //NavigationUI.setupActionBarWithNavController(this, navController, linearLayout)
-            //NavigationUI.setupWithNavController(binding.navView, navController)
-
-            //val toasted = Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT)
-            // Show toast
-            //toasted.show()
+        // TODO init httprequests here
+        
+        //val toasted = Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT)
+        // Show toast
+        //toasted.show()
     }
+
     // todo create and inflate menu here
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
         val inflater = menuInflater
+
         inflater.inflate(R.menu.my_menubar, menu)
         // return true so that the menu pop up is opened
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(this, R.id.navigation)) || super.onOptionsItemSelected(item)
-        /*when (item.itemId) {
+        return when (item.itemId) {
             R.id.logo -> {
-                //goto fragment, if username is set
-                //showStatus()
-
+                //Show status screen
+                showStatus()
+                // return true so that the menu pop up is opened
+                true
             }
             R.id.logs -> {
-                //goto fragment, if username is set
-                //showLogs()
+                // Show log screen
+                showLogs()
+                // return true so that the menu pop up is opened
+                true
             }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
-        */
     }
 
-    /*override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.navigation)
-        return NavigationUI.navigateUp(navController, linearLayout)
-    }*/
+    private fun showLogs() {
+        Log.d("Logs", "ShowLogs in main")
+    }
+
+    private fun showStatus() {
+        Log.d("Status", "ShowStatus in main")
+    }
 }
