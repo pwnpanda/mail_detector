80bc6e3b91a9c092ea6a4119c3c2876e646ed50e
Robin Lunde <robin@teamcyber.no>
2021-03-26 22:38:48 +0100

    Working fragments

--- Android/app/src/main/java/com/robinlunde/mailbox/login/LoginFragment.kt
+++ Android/app/src/main/java/com/robinlunde/mailbox/login/LoginFragment.kt
@@ -1,12 +1,15 @@
 package com.robinlunde.mailbox.login
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.robinlunde.mailbox.OnFragmentInteractionListener
 import com.robinlunde.mailbox.R
 import com.robinlunde.mailbox.databinding.FragmentLoginBinding
 
@@ -15,6 +18,7 @@ import com.robinlunde.mailbox.databinding.FragmentLoginBinding
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var myListener : OnFragmentInteractionListener
 
 /**
  * A simple [Fragment] subclass.
@@ -27,7 +31,8 @@ class LoginFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
 
         binding.usernameButton.setOnClickListener { view: View ->
-            // No navcontroller set! Find out how to set one! TODO
+            val imm: InputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
             view.findNavController().navigate(R.id.action_loginFragment_to_alertFragment)
         }
 
@@ -44,7 +49,14 @@ class LoginFragment : Fragment() {
 
         binding.lifecycleOwner = this*/
 
-        // Inflate the layout for this fragment
-        // return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            myListener = activity as OnFragmentInteractionListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener")
+        }
     }
 }
