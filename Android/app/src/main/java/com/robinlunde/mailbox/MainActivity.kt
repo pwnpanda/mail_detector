fad7aa7e072d2f7d105028b3fbe0e01bb132095a
Robin Lunde <robin@teamcyber.no>
2021-03-29 23:25:16 +0200

    Generalized UI and added notifications for testing

--- Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
+++ Android/app/src/main/java/com/robinlunde/mailbox/MainActivity.kt
@@ -15,7 +15,6 @@ import com.robinlunde.mailbox.databinding.ActivityMainBinding
 class MainActivity() : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
