09f192df2d98c0e82cfbf2fbc918cae3402e5ac6
Robin Lunde <robin@wub.no>
2021-04-11 21:33:51 +0200

    Working BT value reading at startup. Better color config

--- Android/app/src/main/java/com/robinlunde/mailbox/network/BlueToothLib.kt
+++ Android/app/src/main/java/com/robinlunde/mailbox/network/BlueToothLib.kt
@@ -3,18 +3,24 @@ package com.robinlunde.mailbox.network
 import android.util.Log
 import com.juul.kable.Advertisement
 import com.juul.kable.Scanner
+import com.juul.kable.characteristicOf
+import com.juul.kable.peripheral
 import com.robinlunde.mailbox.MailboxApp
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import java.util.concurrent.TimeUnit
 
 private val SCAN_DURATION_MILLIS = TimeUnit.SECONDS.toMillis(10)
+private val SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+private val CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+private val dataConfigCharacteristics = characteristicOf(SERVICE_UUID, CHARACTERISTIC_UUID)
 
 sealed class ScanStatus {
     object Stopped : ScanStatus()
     object Started : ScanStatus()
     data class Failed(val message: CharSequence) : ScanStatus()
 }
+
 
 /**
  * https://github.com/JuulLabs/sensortag/blob/14dc04c1aa7eafdbcb6c2bbbc4ec063f9276c42e/app/src/androidMain/kotlin/features/scan/ScanViewModel.kt
@@ -49,16 +55,18 @@ class BlueToothLib {
                 scanner
                     .advertisements
                     .catch { cause ->
-                        Log.d("Bluetooth","Scan failed")
-                        _scanStatus.value =
-                            ScanStatus.Failed(cause.message ?: "Unknown error")
+                        Log.d("Bluetooth", "Scan failed")
+                        _scanStatus.value = ScanStatus.Failed(cause.message ?: "Unknown error")
                     }
                     .onCompletion { cause ->
                         Log.d("Bluetooth", "scan over")
                         if (cause == null) _scanStatus.value =
                             ScanStatus.Stopped
                     }
-                    .filter { it.isPi }
+                        // Only keep the relevant data
+                    .filter { it.isFireBeetle }
+                        // If data found, keep data and stop scan
+                        // TODO - Store value, notify UI and API of value (status) and Send response
                     .collect { advertisement ->
                         found[advertisement.address] = advertisement
                         _advertisements.value = found.values.toList()
@@ -77,8 +85,9 @@ class BlueToothLib {
     }
 
     private fun stopScan() {
-        Log.d("Bluetooth","Scan stopped")
-        scanScope.cancelChildren()
+        Log.d("Bluetooth", "Scan stopped")
+        handleContent()
+        //scanScope.cancelChildren()
         _scanStatus.value = ScanStatus.Stopped
     }
 
@@ -86,12 +95,42 @@ class BlueToothLib {
         Log.d("BlueTooth", "Bluetooth has been enabled")
         startScan()
     }
+
+    private fun handleContent() {
+        Log.d("BlueTooth", "Trying to handle content")
+        val peripheral = scanScope.peripheral(advertisement.value[0])
+        Log.d("Bluetooth", "Peripheral $peripheral created")
+        scanScope.launch {
+            peripheral.connect()
+            Log.d("BlueTOOTH", "Connected to $peripheral")
+
+            val data: String = runBlocking { withContext(Dispatchers.IO) { peripheral.read(dataConfigCharacteristics).decodeToString() } }
+            Log.d("Bluetooth", "First data: $data")
+
+            try {
+                peripheral.observe(dataConfigCharacteristics).collect { data ->
+                    Log.d("Bluetooth", "Data update: ${data.decodeToString()}")
+                }
+            } catch (e: Exception){
+                 Log.d("Bluetooth", "Exception $e")
+             }
+           
+            /*peripheral.state.collect { state: State ->
+                Log.d("Bluetooth", "State changed to $state for peripheral $peripheral")
+            }*/
+        }
+    }
 }
 
 // How to identify correct node. TODO Need to update for different boards
 private val Advertisement.isPi
     get() = name?.startsWith("RaspberryPi") == true ||
             name?.startsWith("Pi") == true || address == "B8:27:EB:12:D0:9A"
+
+// How to identify correct node. TODO Need to update for different boards
+private val Advertisement.isFireBeetle
+    get() = name?.startsWith("FireBeetle") == true ||
+            name?.startsWith("Robin") == true || address == "7C:9E:BD:D9:E1:92"
 
 private fun CoroutineScope.childScope() =
     CoroutineScope(coroutineContext + Job(coroutineContext[Job]))
