e4e62c773c1c8bae7f04c9293ba2ab80a62d2e42
Robin Lunde <robin@wub.no>
2021-04-11 23:22:10 +0200

    FireBeetle can now send data to phone

--- FireBeetle/ble_server.ino
+++ FireBeetle/ble_server.ino
@@ -7,29 +7,57 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <stdio.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+BLEServer *pServer;
+BLEService *pService;
+BLECharacteristic *pCharacteristic;
+char *buf = new char[11];
+
+void setSendValue(){
+  Serial.print(F("Setting current value: "));
+  Serial.println(buf);
+  pCharacteristic->setValue(buf);
+  pCharacteristic->notify();
+}
+
+unsigned long getCurrent(){
+  return millis();
+  /*  How to calculate offset if overflow occurs
+   * if (nowTime < before){
+   *   sum = (MAX_UNSIGNED_LONG - before) + nowTime
+   * }
+   */
+}
+
+char *getCurrentAsString(){
+  ltoa(getCurrent(), buf, 10);
+  Serial.print(F("Buffer is: "));
+  Serial.println(buf);
+  return buf;
+  //return std::to_string(getCurrent());
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  Serial.println(F("Starting BLE work!"));
 
   BLEDevice::init("FireBeetle ESP32-E Robin");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  pCharacteristic->setValue("ThisIsTheValue");
+  pCharacteristic->setValue(getCurrentAsString());
   pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
@@ -40,6 +68,8 @@ void setup() {
 }
 
 void loop() {
+  getCurrentAsString();
+  setSendValue();
   // put your main code here, to run repeatedly:
   delay(2000);
 }
