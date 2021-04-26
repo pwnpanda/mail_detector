a620d8463f13495d809fa8828917700dde090310
Robin Lunde <robin@wub.no>
2021-04-26 23:22:56 +0200

    Working communication between device and arduino. Chip needs error handling for old or broken connections. Android needs nullpointer handling when chip disconnects

--- FireBeetle/ble_server.ino
+++ FireBeetle/ble_server.ino
@@ -16,14 +16,16 @@
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 BLEServer *pServer;
 BLEService *pService;
+BLEAdvertising *pAdvertising;
 BLECharacteristic *pCharacteristic;
+bool connected = false;
 char *buf = new char[11];
 
 void setSendValue(){
   Serial.print(F("Setting current value: "));
   Serial.println(buf);
   pCharacteristic->setValue(buf);
-  pCharacteristic->indicate();
+  pCharacteristic->notify();
 }
 
 unsigned long getCurrent(){
@@ -42,6 +44,41 @@ char *getCurrentAsString(){
   return buf;
   //return std::to_string(getCurrent());
 }
+class MyCallbacks: public BLECharacteristicCallbacks {
+  // Someone connected
+  void onConnect (BLEServer *pServer){
+    connected = true;
+    pAdvertising->stop();
+    Serial.println("Device connected!");
+  }
+
+  // Someone disconnected
+  void onDisconnect (BLEServer *pServer){
+    connected = false;
+    pAdvertising->start();
+    Serial.println("Device disconnected!");
+  }
+
+  // When someone writes to characteristic
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+    // Make sure we have data
+    if (value.length() > 0) {
+      Serial.println("Characteristic was written! New value: ");
+      // Print all data
+      int res = (int) value[0]
+      if (res == 1) Serial.println("Value = 1 received, going to sleep!");
+      // for (int i = 0; i < value.length(); i++)  Serial.printf("\nStr: %s, Char: %c, D - int: %d, X - hex: %x A - double as hex: %a, E - double: %e", value[i], value[i], value[i], value[i], value[i], value[i]);
+    } else {
+      Serial.println("Something went wrong when reading data from characteristic. Length is 0!");
+    }
+    Serial.println("****************************************");
+    delay(15000);
+    // goto sleep!
+  }
+};
+
+// todo needs error-handling to flush connections in case of disconnect!
 
 void setup() {
   Serial.begin(115200);
@@ -52,18 +89,21 @@ void setup() {
   pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::NOTIFY |
-                                         BLECharacteristic::PROPERTY_INDICATION
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE |
+                                         BLECharacteristic::PROPERTY_NOTIFY /*|
+                                         BLECharacteristic::PROPERTY_INDICATE*/
                                        );
-
+  pCharacteristic->setCallbacks(new MyCallbacks());
   pCharacteristic->setValue(getCurrentAsString());
   pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
-  BLEDevice::startAdvertising();
+  pAdvertising->start();
+  // BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
@@ -71,5 +111,5 @@ void loop() {
   getCurrentAsString();
   setSendValue();
   // put your main code here, to run repeatedly:
-  delay(2000);
+  delay(5000);
 }
