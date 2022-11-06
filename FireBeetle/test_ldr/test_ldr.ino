24ce089ff135a52f45a59d2fb22c225ffa07d879
Robin Lunde <robin@wub.no>
2022-11-06 21:05:53 +0100

    Minor android changes and nice arduino things

--- FireBeetle/test_ldr/test_ldr.ino
+++ FireBeetle/test_ldr/test_ldr.ino
@@ -4,6 +4,22 @@ const int buttonPin = 25;
 #define GPIO_LEVEL 0  // If wakeup is on high or low edge (0 means wakeup will trigger when value is ~0)
 RTC_DATA_ATTR int runNr = 0;
 
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+
 int val = 0;
 void setup() {
 	Serial.begin(115200);
@@ -14,6 +30,10 @@ void setup() {
     // Set sleep wakeup condition - WakeUp if GPIO Goes to level set in GPIO_LEVEL!
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_25, GPIO_LEVEL);
   
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
   int i = 0;
 
   while(i<5){
